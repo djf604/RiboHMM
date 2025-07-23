a111dba67443f138c1e3660dbf512f3f203b3b86
djf604 <11505825+djf604@users.noreply.github.com>
2025-07-22 22:04:14 -0500

    Temporary setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RiboHMM',
+    version='2.1.0',
+    description='Algorithm for inferring coding sequences from Riboseq data',
+    license='Apache',
+    author='Dominic Fitzgerald/Anil Raj',
+    author_email='dominicfitzgerald11@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'ribohmm': ['include/*']
+    },
+    entry_points={
+        'console_scripts': [
+            'ribohmm = ribohmm:execute_from_command_line',
+            'ribohmm-utils = ribohmm:execute_utils_from_command_line'
+        ]
+    },
+    install_requires=['pysam', 'cvxopt', 'numpy', 'scipy', 'dill', 'pandas'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
